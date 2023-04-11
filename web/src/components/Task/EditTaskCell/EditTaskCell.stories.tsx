41786492279e70ef3d39db69b3849b3580dd2d86
Victoria McCrady <spartanginger@gmail.com>
2023-04-10 17:32:07 -0700

    docs: fix sotrybook so it can render components, complete generated edit task cell in storybook
    install tailwind and remove webpack errors

--- web/src/components/Task/EditTaskCell/EditTaskCell.stories.tsx
+++ web/src/components/Task/EditTaskCell/EditTaskCell.stories.tsx
@@ -1,6 +1,18 @@
 import type {ComponentStory} from '@storybook/react'
-import EditTaskCell from '.'
 
-export const generated: ComponentStory<typeof EditTaskCell> = (args) => {
-  return <EditTaskCell {...args} />
+import {Loading,Success,Failure} from './EditTaskCell'
+import { standard } from './EditTaskCell.mock'
+
+export const loading = () => {
+  return <Loading />
 }
+
+export const success = () => {
+  return <Success task={standard().one}/>
+}
+
+export const failure = () => {
+  return <Failure error={new Error("oh no! and error")} />
+}
+
+export default { title: 'Cells/EditTaskCell' }
